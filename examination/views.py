8cff0853c47d4f278595f6b1a1be08875df59474
yankee-co <zimae.9525@gmail.com>
2021-02-03 21:57:17 +0200

    fixed

--- examination/views.py
+++ examination/views.py
@@ -7,32 +7,36 @@ from .models import Transaction
 class HomePageView(TemplateView):
     def get(self, *args, **kwargs):
 
-        listed_data = []
+        data = []
         dicted = {}
         number = 0
 
-        for transaction in Transaction.objects.all():
+        transactions = Transaction.objects.all()
+
+        for transaction in transactions:
+
+            print(transaction.amount)
 
             number += 1
             sender = transaction.credit_account.client.first_name + ' ' + transaction.credit_account.client.last_name
             getter = transaction.debit_account.client.first_name + ' ' + transaction.debit_account.client.last_name
             date = str(transaction.created.date()) + ' ' + str(transaction.created.time())
 
-            dicted.update({
+            dicted = {
             'number' : number,
             'type' : transaction.transaction_type.name,
             'date' : date,
             'amount' : transaction.amount / 100,
             'sender' : sender,
             'getter' : getter,
-            })
+            }
 
-            listed_data.append(dicted)
-
+            data.append(dicted)
+            print(dicted)
         return render(
             request = self.request,
             template_name = 'home_page.html',
             context={
-            'listed_data' : listed_data,
+            'data' : data,
             }
             )
